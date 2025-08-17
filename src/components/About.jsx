1153b1b398cc71db450e2a6480ef98bc90276b3d
Sudarshan P <107467585+sudarshan441@users.noreply.github.com>
2025-08-17 14:51:21 +0530

    updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import myResume from '../assets/Sudarshan-Resume-fw20_0365.pdf'
+import myResume from "../assets/SudarshanPujari-Resume.pdf";
 const About = () => {
   const info = [
     { text: "Years experience", count: "04" },
@@ -17,17 +17,43 @@ const About = () => {
           <div className="p-2">
             <div className="text-gray-300 my-3">
               <p className="text-justify leading-7 w-11/12 mx-auto">
-              I am a B.Tech graduate in EEE, I love exploring new technologies in the field of Web Development and always try to adapt to it.I believe in the concept of continuous learning by regularly upgrading my skills and enhancing my knowledge.I have learned MERN stack, data structures, algorithms, and soft skills at Masai School.And besides coding, I love listening to music, rapping and As a developer, my hunger for learning has drastically increased. And I want to centralize my skills and learning for enhancement of an organization and more of it, to enhance my career.
+                Hey! I’m Sudarshan Pujari, a Full Stack Web Developer who loves
+                turning ideas into fast, functional, and visually stunning web
+                applications. I started as a B.Tech graduate in Electrical &
+                Electronics Engineering, but my real spark ignited when I
+                discovered web development, and I haven’t looked back since. I
+                specialize in the MERN stack and work confidently with tools
+                like Next.js, Tailwind CSS, HTMX, Alpine.js, and Golang. From
+                building secure backends with JWT & Payload CMS, to deploying
+                slick frontends on Vercel and AWS, I enjoy bringing full-stack
+                solutions to life. I’ve worked with startups like Evfy and
+                Auditcue, where I built scalable systems, automated workflows
+                using Gemini AI, and focused on clean, reusable code. I believe
+                in continuous learning, clean coding practices, and staying on
+                top of emerging tech not just because it’s essential, but
+                because I genuinely enjoy it. Let’s build something impactful
+                together.
               </p>
-             
               <br />
               <br />
-              <a href={myResume} download={"Sudarshan-Resume-fw20_0365.pdf"} target={"_blank"}>
-               <button style={{backgroundColor:"rgb(8 145 178 / var(--tw-bg-opacity))",padding:"12px",borderRadius:"20px",color:"white"}}>Download CV</button>
-             </a>
+              <a
+                href={myResume}
+                download={"SudarshanPujari-Resume.pdf"}
+                target={"_blank"}
+              >
+                <button
+                  style={{
+                    backgroundColor: "rgb(8 145 178 / var(--tw-bg-opacity))",
+                    padding: "12px",
+                    borderRadius: "20px",
+                    color: "white",
+                  }}
+                >
+                  Download CV
+                </button>
+              </a>
             </div>
           </div>
-          
         </div>
       </div>
     </section>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,11 @@
-import myResume from '../assets/Sudarshan-Resume-fw20_0365.pdf'
+import myResume from '../assets/SudarshanPujari-Resume.pdf'
 export default function Resume(){
     return(
         <section id="resume"  className="py-10 text-white">
           <h3 className="text-4xl font-semibold">
           RESU<span className="text-cyan-600">ME</span>
         </h3>
-         <a href={myResume} download={"Sudarshan-Resume-fw20_0365.pdf"} target={"_blank"}>
+         <a href={myResume} download={"SudarshanPujari-Resume.pdf"} target={"_blank"}>
            <button style={{backgroundColor:"rgb(8 145 178 / var(--tw-bg-opacity))",padding:"12px",borderRadius:"20px",color:"white",marginTop:"100px"}}>Download cv</button>
          </a> 
            
